198930e006318ca1832216d44e9e362267a4656e
Madihson <bamadoutoure@gmail.com>
2025-08-08 14:42:02 +0000

    Mon projet

---
+++ script.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector("button");
+let div = document.querySelector("#liste-eleves");
+
+let recupJson = async () => {
+  let result = await fetch('externe/eleves_bulletin.json'); // vérifie que le chemin est correct
+  let data = await result.json();
+
+  let contenu = "";
+
+  data.forEach(eleve => {
+    contenu += `
+      <div class="col-md-4 col-sm-6">
+        <div class="card h-100 shadow-sm border-0">
+          <div class="card-body">
+            <h5 class="card-title">${eleve.prenom} ${eleve.nom}</h5>
+            <div class="card-text">
+              Moyenne : <strong>${eleve.moyenne}</strong><br>
+              Rang : <strong>${eleve.rang}</strong><br>
+              Appréciation : <em>${eleve.appreciation}</em>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  div.innerHTML = contenu;
+
+
+};
+
+btn.addEventListener("click", recupJson);
+
